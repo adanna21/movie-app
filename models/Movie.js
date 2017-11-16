bc3a618789070288d5a6ce48b8f6ab9c9bc7ac02
Adanna Omatu <adaomatu@gmail.com>
2017-11-16 11:34:38 -0500

    creaated update and destroy methods in model

--- models/Movie.js
+++ models/Movie.js
@@ -21,3 +21,24 @@ Movie.create = (movie, userId) => {
     RETURNING *
     `, [movie.title, movie.description, movie.genre, userId]);
 }
+
+Movie.update = (movie, id) => {
+  return db.one(`
+    UPDATE movies SET
+    title = $1,
+    description = $2,
+    genre = $3
+    WHERE id = $4
+    RETURNING *
+    `, [movie.title, movie.description, movie.genre, id]);
+}
+
+Movie.destroy = (id) => {
+  return db.one(`
+    DELETE FROM movies
+    WHERE id = $1
+    `, [id]);
+}
+
+
+module.exports = Movie;
