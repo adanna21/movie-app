0c0fa67fe679cb8d84f1a71e774b9a3dbd66e46f
Danny Eng <dannyeng94@gmail.com>
2017-11-16 10:24:35 -0500

    imported components into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 class App extends Component {
 
   // constructor & state
