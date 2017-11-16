2f2a7a0cef595a338f37b8661fe568750814455c
Adanna Omatu <adaomatu@gmail.com>
2017-11-16 15:49:24 -0500

    js formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,126 +1,124 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import Register from './components/Register';
-import './App.css';
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
+import Login from './components/Login'
+import Dashboard from './components/Dashboard'
+import Register from './components/Register'
+import './App.css'
 
-import Home from './components/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Home from './components/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import MovieList from './components/MovieList'
+const fetch = window.fetch
 
 class App extends Component {
-
   // constructor & state
-  constructor(props){
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       auth: false,
-      user: null,
+      user: null
     }
-    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
-    this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
-    this.logout = this.logout.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this)
+    this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this)
+    this.logout = this.logout.bind(this)
   }
 
-  componentDidMount(){
+  componentDidMount () {
     fetch('/api/auth/verify', {
-      credentials: 'include',
+      credentials: 'include'
     }).then(res => res.json())
     .then(res => {
       this.setState({
         auth: res.auth,
-        user: res.data.user,
+        user: res.data.user
       })
-    }).catch(err => console.log(err));
+    }).catch(err => console.log(err))
   }
 
-  handleLoginSubmit(e, data){
-    e.preventDefault();
+  handleLoginSubmit (e, data) {
+    e.preventDefault()
     fetch('/api/auth/login', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       credentials: 'include',
       body: JSON.stringify(data)
     }).then(res => res.json())
     .then(res => {
-      console.log(res);
+      console.log(res)
       this.setState({
         auth: res.auth,
-        user: res.data.user,
+        user: res.data.user
       })
-    }).catch(err => console.log(err));
+    }).catch(err => console.log(err))
   }
 
-  handleRegisterSubmit(e, data){
-    console.log(data);
-    e.preventDefault();
+  handleRegisterSubmit (e, data) {
+    console.log(data)
+    e.preventDefault()
     fetch('/api/auth/register', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       credentials: 'include',
       body: JSON.stringify(data)
     }).then(res => res.json())
     .then(res => {
-      console.log(res);
+      console.log(res)
       this.setState({
         auth: res.auth,
-        user: res.data.user,
+        user: res.data.user
       })
-    }).catch(err => console.log(err));
+    }).catch(err => console.log(err))
   }
 
-  logout(){
+  logout () {
     fetch('/api/auth/logout', {
-      credentials: 'include',
+      credentials: 'include'
     }).then(res => res.json())
     .then(res => {
       this.setState({
-        auth: res.auth,
+        auth: res.auth
       })
-    }).catch(err => console.log(err));
+    }).catch(err => console.log(err))
   }
 
-  render() {
+  render () {
     return (
       <Router>
-        <div className="App">
+        <div className='App'>
           <Header
             logout={this.logout}
           />
-          <div className="container">
-            <Route path="/" component={Home} />
-            <Route exact path="/login" render={() => (
+          <div className='container'>
+            <Route path='/' component={Home} />
+            <Route exact path='/login' render={() => (
               this.state.auth
-              ? <Redirect to="/dashboard" />
+              ? <Redirect to='/dashboard' />
               : <Login handleLoginSubmit={this.handleLoginSubmit} />
             )} />
-            <Route exact path="/dashboard" render={() => (
+            <Route exact path='/dashboard' render={() => (
               !this.state.auth
-              ? <Redirect to="/login" />
+              ? <Redirect to='/login' />
               : <Dashboard user={this.state.user} />
             )} />
-            <Route exact path="/register" render={() => (
+            <Route exact path='/register' render={() => (
               this.state.auth
-              ? <Redirect to="/dashboard" />
+              ? <Redirect to='/dashboard' />
               : <Register handleRegisterSubmit={this.handleRegisterSubmit} />
             )} />
-          <Route exact path='/movies' render={() => <MovieList auth={this.state.auth} />} />
+            <Route exact path='/movies' render={() => <MovieList auth={this.state.auth} />} />
 
           </div>
           <Footer />
 
-
         </div>
       </Router>
-    );
+    )
   }
-
 }
 
-export default App;
+export default App
