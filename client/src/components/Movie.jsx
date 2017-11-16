797479c283019cba917631878bc575943c1e57ac
marco acosta <marcucho14@hotmail.com>
2017-11-16 11:33:17 -0500

    completed Movie component.

---
+++ client/src/components/Movie.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Movie = (props) => {
+  return(
+    <div className="Movie">
+      <h3>{props.movie.title}</h3>
+      <h4>{props.movie.genre}</h4>
+      <p>{props.movie.description}</p>
+    </div>
+  );
+};
+
+
+export default Movie;
