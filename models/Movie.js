96fd1c8f0036b8c8b434f1a6aafd42fc6392c112
Adanna Omatu <adaomatu@gmail.com>
2017-11-16 11:29:00 -0500

    created the findAll, findById, create methods in model

---
+++ models/Movie.js
@@ -0,0 +1,23 @@
+const db = require('../db/config');
+
+const Movie = {};
+
+Movie.findAll = () => {
+  return db.query('SELECT * FROM movies');
+}
+
+Movie.findById = (id) => {
+  return db.oneOrNone(`
+    SELECT * FROM movies
+    WHERE id = $1
+    `, [id]);
+}
+
+Movie.create = (movie, userId) => {
+  return db.one(`
+    INSERT INTO movies
+    (title, description, genre, user_id)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+    `, [movie.title, movie.description, movie.genre, userId]);
+}
